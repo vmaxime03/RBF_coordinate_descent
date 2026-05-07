26d648a5a29f47f1d13190bed404627a76aaf616
vmaxime <mister.max0303@gmail.com>
2026-05-07 11:05:32 +0200

    stop optimization on convergence

--- src/main.cpp
+++ src/main.cpp
@@ -14,39 +14,10 @@
 #include "rbf.hpp"
 #include "test_init.hpp"
 #include "sdf.hpp"
+#include "output.hpp"
 
 using namespace UM;
 
-void debug_sdf(SDF& sdf, double minx, double miny, double maxx, double maxy, const std::string& fname, int step = 100) {
-	std::ofstream out(fname);
-	int w = step;
-	int h = step;
-	for (int j = 0; j < h; ++j) {
-		for (int i = 0; i < w; ++i) {
-			const double x = minx + (double(i) / double(w - 1)) * (maxx - minx);
-			const double y = miny + (double(j) / double(h - 1)) * (maxy - miny);
-			double dist = sdf.distance({x, y});
-			vec2 grad = sdf.gradient({x, y});
-			out << x << "," << y << "," << dist << "," << grad.x << "," << grad.y ;
-
-			if (i < w - 1) out << ",";
-		}
-		out << "\n";
-	}
-	out.close();
-}
-
-void export_polyline(PolyLine& pl, const std::string& fname) {
-	std::ofstream out(fname);
-	for (const auto& e : pl.iter_edges()) {
-		auto f = e.from().pos(), t = e.to().pos();
-		out << f.x << "," << f.y << "," << t.x << "," << t.y << "\n";
-	}
-	out.close();
-}
-
-
-// =========================================================================
 
 typedef std::pair<vec2, vec2> PointNormal;
 typedef std::vector<PointNormal> Samples;
@@ -58,7 +29,7 @@ void  compute_samples_normals(PolyLine& pl, Samples& samples, size_t n = 10) {
 		
 		vec2 normal = vec2(-d.y, d.x).normalized();
 
-		for (size_t i = 1; i < n-1; ++i) {
+		for (size_t i = 0; i < n; ++i) {
 			vec2 p = e.from().pos().xy() + ((double)i / (double)n) * d;
 			samples.push_back({p, normal});
 
@@ -71,6 +42,10 @@ double error(SDF& sdf, PointNormal& p) {
 	vec2 g = sdf.gradient(p.first); // gradiant
 	
 	vec2 diff = (g - p.second);
+
+	// || grad f || = 1
+	double eikonal = g.norm() - 1.;
+
 
 	// pi on polyline 
 	// distance -> 0 
@@ -137,31 +112,32 @@ int main(int argc, char** argv) {
 	auto rbf = std::make_unique<Gaussian>();
 	SDF sdf(std::move(rbf));
 
-	PolyLineGenerator::regular_polygon(pl, sdf, 3, 5);
+	//PolyLineGenerator::regular_polygon(pl, sdf, 8, 6);
 	
-	//PolyLineGenerator::random_polygon(pl, sdf, 15, 5);
+	PolyLineGenerator::random_polygon(pl, sdf, 8, 5);
 	
 	//PolyLineGenerator::read_from_file(pl, sdf, input_dir + "duck.geogram", 50);
 
 
 	// 	   lower bound		upper bound 	step
 	double lb_point = -5.,  ub_point = 5.,  	step_point = 0.01;
-	double lb_alpha =  0.,  ub_alpha = 10., 	step_alpha = 0.01;
+	double lb_alpha =  -5.,  ub_alpha = 10., 	step_alpha = 0.01;
 	double lb_beta  = -5.,  ub_beta  = 5.,  	step_beta  = 0.01;
 	double lb_sigma = 0.5,	ub_sigma = 10.,  	step_sigma = 0.01;
 
 
 
 
-	size_t max_it = (argc > 1) ? std::stoul(argv[1]) : 500;
+	size_t max_it = (argc > 1) ? std::stoul(argv[1]) : 1000;
 
 	Samples samples;
 	compute_samples_normals(pl, samples, 10);
 
+	bool c = true;
 	for (size_t it = 0; it < max_it; ++it) {
 
-		
-		if (it % std::max<size_t>(1, max_it / 20) == 0) {
+			
+		if (it % std::max<size_t>(1, max_it / 20) == 0 || !c) {
 			double total_err = error_total(sdf, samples);
 			std::cout << it << ": err : " << total_err << "\t"  << sdf.to_string() << std::endl;
 
@@ -169,18 +145,25 @@ int main(int argc, char** argv) {
 			std::ofstream(output_dir + std::to_string(it) + "sdf.json") << sdf.to_json().dump(2);
 		}
 
+		if (!c) break;
+		c = false;
+
 		for (size_t i = 0; i < sdf.p.size(); ++i) {
 
 
 			double ce = error_total(sdf, samples);
 
-			try_step(sdf.alpha[i],  step_alpha, lb_alpha, ub_alpha, sdf, i, samples, ce);
-			try_step(sdf.beta[i].x, step_beta,  lb_beta,  ub_beta,  sdf, i, samples, ce);
-			try_step(sdf.beta[i].y, step_beta,  lb_beta,  ub_beta,  sdf, i, samples, ce);
-			try_step(sdf.p[i].x,    step_point, lb_point, ub_point, sdf, i, samples, ce);
-			try_step(sdf.p[i].y,    step_point, lb_point, ub_point, sdf, i, samples, ce);
-			try_step(sdf.sigma[i],  step_sigma, lb_sigma, ub_sigma, sdf, i, samples, ce);
+
+			c |= try_step(sdf.alpha[i],  step_alpha, lb_alpha, ub_alpha, sdf, i, samples, ce);
+			c |= try_step(sdf.beta[i].x, step_beta,  lb_beta,  ub_beta,  sdf, i, samples, ce);
+			c |=try_step(sdf.beta[i].y, step_beta,  lb_beta,  ub_beta,  sdf, i, samples, ce);
+			c |= try_step(sdf.p[i].x,    step_point, lb_point, ub_point, sdf, i, samples, ce);
+			c |= try_step(sdf.p[i].y,    step_point, lb_point, ub_point, sdf, i, samples, ce);
+			c |= try_step(sdf.sigma[i],  step_sigma, lb_sigma, ub_sigma, sdf, i, samples, ce);
+
+			
 		}
+
 	}
 
 	export_polyline(pl, output_dir + "polyline.csv");
