dd9658d631a0eaa7a5a032d2dc1af98a1a182707
vmaxime <mister.max0303@gmail.com>
2026-05-22 17:19:02 +0200

    update anim

--- src/script/animation.py
+++ src/script/animation.py
@@ -135,15 +135,16 @@ def main():
         ax.plot(px, py, color="lime", linewidth=2, zorder=4)
 
         # normals
-        for mx, my, nx, ny in normals:
-            arr = FancyArrowPatch(
-                (mx, my),
-                (mx + nx * 0.15, my + ny * 0.15),
-                arrowstyle="-|>", color="cyan",
-                mutation_scale=8, linewidth=1.2, zorder=5,
-                alpha=0.8,
-            )
-            ax.add_patch(arr)
+       #
+       #for mx, my, nx, ny in normals:
+       #    arr = FancyArrowPatch(
+       #        (mx, my),
+       #        (mx + nx * 0.15, my + ny * 0.15),
+       #        arrowstyle="-|>", color="cyan",
+       #        mutation_scale=8, linewidth=1.2, zorder=5,
+       #        alpha=0.8,
+       #    )
+       #    ax.add_patch(arr)
 
 
          
